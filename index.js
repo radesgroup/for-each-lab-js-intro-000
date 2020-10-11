0db726b27564a78ab0aee5a06c839294c69fe88e
Aleksey <fduchunru@gmail.com>
2020-10-11 23:15:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function iterativeLog(array){
+  array.forEach(element => console.log(element))
+}
+
+
+function iterate(callback){
+
+}
+
+
+function doToArray(array, callback){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+// log out animals -- pretty cool, right?
+console.log(animals);*/
